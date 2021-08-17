ba3adec2ab6c115ad147e46b01d680c113e90065
tylers1st <tylerscott1st@gmail.com>
2021-08-17 15:12:07 -0500

    added warning

--- image-randomizer.js
+++ image-randomizer.js
@@ -1,24 +1,29 @@
-let tok = canvas.tokens.controlled[0];
-let tokName = token.name.split(' ', 2);
-tokName = tokName[0];
+if(canvas.tokens.controlled.length == 1){
+  let tok = canvas.tokens.controlled[0];
+  let tokName = token.name.split(' ', 2);
+  tokName = tokName[0];
 
-let dir = tok.data.img.split('/');
-dir.splice(dir.length - 1, 1);
+  let dir = tok.data.img.split('/');
+  dir.splice(dir.length - 1, 1);
 
-dir = dir.join('/');
+  dir = dir.join('/');
 
-let content = await FilePicker.browse("user", dir);
+  let content = await FilePicker.browse("user", dir);
 
-let min = Math.ceil(0);
-let max = Math.floor(content.files.length);
+  let min = Math.ceil(0);
+  let max = Math.floor(content.files.length);
 
-let newImage = content.files[Math.floor(Math.random() * (max - min) + min)];
-const update = [];
+  let newImage = content.files[Math.floor(Math.random() * (max - min) + min)];
+  const update = [];
 
-update.push({
+  update.push({
     _id: tok.id,
     img: newImage
-});
-console.log(newImage);
+  });
+  console.log(newImage);
 
-canvas.tokens.updateMany(update);
+  canvas.tokens.updateMany(update);
+}
+else{
+  ui.notifications.warn("You may only select one token.")
+}
